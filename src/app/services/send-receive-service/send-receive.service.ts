476418735bf91aec314ef73eee3ba1c9f2588d92
Alex Rirak <alexrirak@yahoo.com>
2022-02-26 01:11:36 -0500

    ui updates

--- src/app/services/send-receive-service/send-receive.service.ts
+++ src/app/services/send-receive-service/send-receive.service.ts
@@ -1,23 +1,13 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+
 @Injectable()
 export class SendReceiveApiService {
-    constructor(private http: HttpClient){
+	constructor(private http: HttpClient) {
 
-    }
-    
-    getSendReceiveInitData(param1: any){
-        //not actually required
-        // @ts-ignore
-        const httpOptions = {
-            headers: new HttpHeaders({
-              'Content-Type':  'application/json'
-            })
-          };
-        var postData = {
-            "param1": param1
-        }
-        return this.http.post("/CryptoBanksters/SendReceive/InitData", postData).pipe(map(data => data))
-    }
+	}
+
+	getSendReceiveInitData(user: string) {
+		return this.http.get("http://crypto-banksters-wallet-api.azurewebsites.net/wallets/user/" + user + "?include_values=True")
+	}
 }
